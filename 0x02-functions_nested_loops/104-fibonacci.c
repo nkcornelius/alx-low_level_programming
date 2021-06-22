f4c7d5f5ce1760cf6715ce060cdea620182a97f2
Cornelius Ngetich <ngetichcornelius0@gmail.com>
2021-06-22 05:33:22 -0700

    Nested Loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -41,7 +41,7 @@ printf("%lu%lu", half1, half2);
 if (count != 98)
 printf(", ");
 
- fib1_half1 = fib2_half1;
+fib1_half1 = fib2_half1;
 fib1_half2 = fib2_half2;
 fib2_half1 = half1;
 fib2_half2 = half2;
