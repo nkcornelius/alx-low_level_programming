8d77247fd290d21337f77f63d9d2c130c26f212e
Cornelius Ngetich <ngetichcornelius0@gmail.com>
2021-07-12 12:22:54 -0700

    C - argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,24 +1,26 @@
+#include <stdio.h>
 #include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * main - multiplies two arguments and prints result
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 if no errors
+ *main - entry
+ *@argv : the string from the terminal
+ *@argc : the number of arameters
+ *Return: 0
  */
-int main(int argc, char *argv[])
-{
-int first, second;
 
-if (argc == 3)
+int main(int argc, char const *argv[])
 {
-first = atoi(argv[1]);
-second = atoi(argv[2]);
-printf("%i\n", first * second);
-return (0);
-}
+if (argc < 3)
+{
 printf("Error\n");
-return (1);
+return (-1);
+}
+else
+{
+int mul;
+mul = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", mul);
+}
+return (0);
 }
