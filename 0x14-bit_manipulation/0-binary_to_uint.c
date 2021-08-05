08f27e5679433fa1821479e86f55914940648a53
Cornelius Ngetich <ngetichcornelius0@gmail.com>
2021-08-05 13:03:07 -0700

    C - Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,61 @@
 #include "holberton.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int.
- * @b: pointer to a string containing a binary number
- *
- * Return: unsigned int with decimal value of binsry number, or 0 if error
+ * powX - raises b to p
+ * @b: base
+ * @p: power
+ * Return: b the power of p
+ */
+int powX(int b, int p)
+{
+int prod = 1;
+
+while (p > 0)
+{
+prod *= b;
+p--;
+}
+return (prod);
+}
+
+/**
+ * _len - length of a string
+ * @s:string
+ * Return: lenght of s
+ */
+int _len(const char *s)
+{
+int len = 0;
+
+while (*s)
+{
+len++;
+s++;
+}
+return (len);
+}
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: input string
+ * Return: unsigned decimal conversion, 0 if input is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-int i;
-unsigned int num;
+int power;
+int num = 0;
 
-num = 0;
 if (!b)
 return (0);
-for (i = 0; b[i] != '\0'; i++)
+power = _len(b) - 1;
+while (*b)
 {
-if (b[i] != '0' && b[i] != '1')
+if (*b != '0' && *b != '1')
 return (0);
-}
-for (i = 0; b[i] != '\0'; i++)
-{
-num <<= 1;
-if (b[i] == '1')
-num += 1
+
+if (*b == '1')
+num += powX(2, power);
+b++;
+power--;
 }
 return (num);
 }
