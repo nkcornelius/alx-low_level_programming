6156fe6e50d6a8e5e515e755b000b2a5b71a3448
Cornelius Ngetich <ngetichcornelius0@gmail.com>
2021-08-09 07:18:23 -0700

    header file

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * elf_header - displays the information contained in the ELF header
+ * @elf_filename: filename of type ELF
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+
+
+}
